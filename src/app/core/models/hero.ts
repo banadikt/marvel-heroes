a6702b874b0bbdea9e1d4234e315891f5f14f2a0
banadikt <banadikt10@gmail.com>
2025-04-14 12:08:42 +0100

    added hero interface

---
+++ src/app/core/models/hero.ts
@@ -0,0 +1,10 @@
+export interface Hero {
+  id: number;
+  nameLabel: string;
+  genderLabel: string;
+  citizenshipLabel: string;
+  skillsLabel: string;
+  occupationLabel: string;
+  memberOfLabel: string;
+  creatorLabel: string;
+}
